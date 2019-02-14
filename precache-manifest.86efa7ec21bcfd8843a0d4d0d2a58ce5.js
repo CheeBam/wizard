a5edaffd1994f672801f647d93b694f3ae8109bd
Dmitry <fantastico@live.ru>
2019-02-14 16:17:32 +0200

    Updates

---
+++ precache-manifest.86efa7ec21bcfd8843a0d4d0d2a58ce5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "672a6146c798e87c4e2a",
+    "url": "/wizard/static/js/main.672a6146.chunk.js"
+  },
+  {
+    "revision": "a0d0828caf1347af18ae",
+    "url": "/wizard/static/css/1.47c862bc.chunk.css"
+  },
+  {
+    "revision": "a0d0828caf1347af18ae",
+    "url": "/wizard/static/js/1.a0d0828c.chunk.js"
+  },
+  {
+    "revision": "ccb37e3f00446d2beabb",
+    "url": "/wizard/static/js/runtime~main.ccb37e3f.js"
+  },
+  {
+    "revision": "11bafda414a3981b09eb7507017e6ed8",
+    "url": "/wizard/static/media/list_logo.11bafda4.svg"
+  },
+  {
+    "revision": "7e9d623db05c8aa46ec4b34453653574",
+    "url": "/wizard/static/media/add.7e9d623d.svg"
+  },
+  {
+    "revision": "8dba8620c80617287d336d053597a655",
+    "url": "/wizard/index.html"
+  }
+];
